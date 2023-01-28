7d34f1baca8ac57db5f4e0dd114d072c333bbbb7
aiden2189 <58379778+aiden2189@users.noreply.github.com>
2023-01-27 22:31:41 -0600

    jammwe

--- Gym app1/Assets/Monday/Destroybutton.cs
+++ Gym app1/Assets/Monday/Destroybutton.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class Destroybutton : MonoBehaviour
 {
     [SerializeField] Editbutton edit1;
+    void Start()
+    {
+        onClick.AddListener(destroy);
+    }
     public void destroy()
     {
         if (edit1.edit2 == true)
